16322920529d856f245df948492e187d336d6ea0
Ben Manekin <ben.manekin@gmail.com>
2019-01-30 18:36:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  batch = []
+  attendees.each { |x| batch << badge_maker(x) }
+  batch
+end
+
+
+def assign_rooms(attendees)
+  attendees.each {
+    puts "Hello #{attendee}! You'll be assigned to room #{index}!"
+  }
+end
