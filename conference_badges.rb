28693361ff9a0625c6a61ed3ada19d18b854b305
Ben Manekin <ben.manekin@gmail.com>
2019-01-30 19:10:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each { |x| puts x }
+  assign_rooms(attendees).each { |x| puts x }
 end
